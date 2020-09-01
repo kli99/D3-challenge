8f6a55faa7db8a77776f8915904b6b7f64ee4e3b
KEFAN LI <1350-kefan.li99@users.noreply.columbia.bootcampcontent.com>
2020-08-31 23:33:53 -0400

    New

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,56 @@
-// @TODO: YOUR CODE HERE!
+// @TODO: YOUR CODE HERE! You need to create a scatter plot between two of the data variables such as 
+// Smokers vs. Age   // Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 660;
+
+// Define the chart's margins as an object
+var chartMargin = {
+    top: 30,
+    right: 30,
+    bottom: 30,
+    left: 30
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3
+    .select("body")
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth);
+
+// Append a group to the SVG area and shift ('translate') it to the right and down to adhere
+// to the margins set in the "chartMargin" object.
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+//Load data from data.csv
+
+d3.csv("data.csv").then(function(healthData) {
+
+    console.log(healthData);
+
+    //log a list of state abbr names
+    var names = healthData.map(data => data.abbr);
+    console.log(names);
+
+    // Cast each age, smokers value in healthData as a number using the unary + operator
+    healthData.forEach(function(data) {
+        data.age = +data.age;
+        data.smokes = +data.smokes;
+        data.smokesLow = +data.smokesLow;
+        data.smokesHigh = +data.smokesHigh;
+
+
+
+        console.log("Smokes:", data.smokes);
+        console.log("LowSmokes:", data.smokesLow);
+        console.log("HighSmokes:", data.smokesHigh);
+        console.log("Age:", data.age);
+    });
+}).catch(function(error) {
+    console.log(error);
+});
